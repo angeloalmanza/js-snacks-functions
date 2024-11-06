0262bf1009818d7ea0bd12d66ca512dd98f379c7
angeloalmanza <angeloalmanza26@gmail.com>
2024-11-06 15:17:04 +0100

    feat: implementazione conta vocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,26 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+/**
+ * funzione che conta il numero di vocali all'interno di una parola
+ * @param {string} parola
+ * @returns {number}
+ */
+const vocalCounter = (parola) => {
+    const vocali = ['a', 'e', 'i', 'o', 'u'];
+    let counter = 0;
+    for(let i = 0; i < parola.length; i++){
+        if(vocali.includes(parola[i].toLowerCase())){
+            counter++;
+        }
+    }
+    return counter;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const vocalNumber = vocalCounter(word);
+console.log(`Il numero delle vocali nella parola ${word} è ${vocalNumber}`);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
