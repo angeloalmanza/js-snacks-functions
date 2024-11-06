d4b0fc9920c92b3d613eee9f632744746c575db5
angeloalmanza <angeloalmanza26@gmail.com>
2024-11-06 15:44:13 +0100

    feat: implementazione bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -9,10 +9,29 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+/**
+ * funzione che genera un messaggio di saluto a seconda dell'ora
+ * @param {string} nome
+ * @returns {string}
+ */
+const saluta = (nome) => {
+    let messagge = '';
+    const time = new Date();
+    const ore = time.getHours();
+    if(ore >= 0 && ore <= 12){
+        messagge = `Buongiorno ${nome}`;
+    }else if(ore >= 13 && ore <= 17){
+        messagge = `Buon pomeriggio ${nome}`;
+    }else{
+        messagge = `Buonasera ${nome}`;
+    }
+    return messagge;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const saluto = saluta(name);
+console.log(saluto);
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
